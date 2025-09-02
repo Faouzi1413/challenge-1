206e9e91f086e72d77c0dde4859b63ecd13ef69a
hichamoubaha <oubahahicham7@gmail.com>
2025-09-02 12:23:05 +0100

    hello

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+int age	
+char sexe, email, nom ;
+printf ("veuillez enrter le nom");
+scanf ("%s",nom);
+printf ("veuillez entrer l'age :");
+printf ("veuillez entre le sexe :");
+scanf ("%s", sexe);
+printf ("veuillez entrer l'adress email");
+scanf ("%s", email);
+
+printf ("le nom est : %S \n ",nom);
+printf ("l'age est : %d \n",age);
+printf ("le sexe est : \n",sexe);
+printf ("l'adress email est :\n",email);
+
+
+return 0 ;
+}
